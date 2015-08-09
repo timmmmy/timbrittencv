b69834da12ab10223a56d5ae8905b3a37595fcb5
timbritten <tim.britten@digital.hmrc.gov.uk>
2015-08-09 22:06:35 +0100

    changed summary

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,7 @@
 */
 
 function initMenus() {
-  
   if (!document.getElementsByTagName) return;
-  
   var aMenus = document.getElementsByTagName("LI");
   for (var i = 0; i < aMenus.length; i++) {
     var mclass = aMenus[i].className;
@@ -15,10 +13,8 @@ function initMenus() {
       var submenu = aMenus[i].childNodes;
       for (var j = 0; j < submenu.length; j++) {
         if (submenu[j].tagName == "A") {
-          
           submenu[j].onclick = function() {
             var node = this.nextSibling;
-                      
             while (1) {
               if (node != null) {
                 if (node.tagName == "UL") {
@@ -34,28 +30,14 @@ function initMenus() {
             }
             return false;
           }
-          
           submenu[j].className = (mclass.indexOf("open") > -1) ? "treeopen" : "treeclosed";
         }
-        
         if (submenu[j].tagName == "UL")
           submenu[j].style.display = (mclass.indexOf("open") > -1) ? "block" : "none";
       }
     }
   }
 }
-
 window.onload = initMenus;
 
-function toggleMe(a){
-var e=document.getElementById(a);
-if(!e)return true;
-if(e.style.display=="none"){
-e.style.display="block"
-}
-else{
-e.style.display="none"
-}
-return true;
-}
 
