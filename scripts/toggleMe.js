09298de29e1936c752b3bbf8558b473bce8e9b0f
timbritten <tim.britten@digital.hmrc.gov.uk>
2015-08-10 18:58:21 +0100

    fixed jscript issues, fixed lists in articles, added up to date content from cv

--- scripts/toggleMe.js
+++ scripts/toggleMe.js
@@ -1,11 +1,27 @@
-function toggleMe(a){
-var e=document.getElementById(a);
-if(!e)return true;
-if(e.style.display=="none"){
-e.style.display="block"
+function forEachElement(selector, fn) {
+  var elements = document.querySelectorAll(selector);
+  for (var i = 0; i < elements.length; i++)
+    fn(elements[i], i);
 }
-else{
-e.style.display="none"
+
+function hideOnLoad () {
+	forEachElement('.hiddenOnLoad', function(el, i){
+		el.style.display="none";
+	});
 }
-return true;
+
+function toggleMe(a) {
+	var e = document.getElementById(a);
+	if(!e) return false;
+
+	if(e.style.display=="none") {
+		e.style.display="block"
+	}
+	else{
+		e.style.display="none"
+	}
+
+	return true;
 }
+
+window.onload = hideOnLoad();
